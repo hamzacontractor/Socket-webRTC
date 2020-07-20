e6cccf6789a109339a09584b11fe164cd97a433f
hamzacontractor <54985043+hamzacontractor@users.noreply.github.com>
2020-07-20 12:17:47 +0530

    Hello World

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+const port = +process.env.PORT || 8080;
+server.listen(port, () => { console.log(`Experess server started on port: ${port}`) })
