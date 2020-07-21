a9e6303028ef4b803cab0d5bd87fb954d576426c
hamzacontractor <54985043+hamzacontractor@users.noreply.github.com>
2020-07-21 15:55:46 +0530

    STUN Server RESET

--- public/js/conference.js
+++ public/js/conference.js
@@ -73,12 +73,13 @@ function callUser(peerID) {
 
 function createPeer(peerID) {
    const peer = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.stunprotocol.org" },
-      {
-         urls: 'turn:numb.viagenie.ca',
-         credential: 'muazkh',
-         username: 'webrtc@live.com'
-      }]
+      iceServers: [
+         { urls: "stun:stun.l.google.com:19302" },
+         { urls: "stun:stun1.l.google.com:19302" },
+         { urls: "stun:stun2.l.google.com:19302" },
+         { urls: "stun:stun3.l.google.com:19302" },
+         { urls: "stun:stun4.l.google.com:19302" }
+      ]
    });
 
    peer.onicecandidate = e => handleICECandidateEvent(e, peerID);
