e40d602fa5358467409ee2b29fae0a6346a8d590
hamzacontractor <54985043+hamzacontractor@users.noreply.github.com>
2020-07-25 22:58:22 +0530

    STUN/TURN server updates

--- public/js/peer.js
+++ public/js/peer.js
@@ -101,7 +101,14 @@ function ToggleVideo() {
 
 function createPeer(peerID) {
    const peer = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.stunprotocol.org" },
+      iceServers: [{
+         urls: "stun:stun.stunprotocol.org",
+         urls: "stun:stun.l.google.com:19302",
+         urls: "stun:stun1.l.google.com:19302",
+         urls: "stun:stun2.l.google.com:19302",
+         urls: "stun:stun3.l.google.com:19302",
+         urls: "stun:stun4.l.google.com:19302"
+      },
       {
          urls: 'turn:numb.viagenie.ca',
          credential: 'muazkh',
